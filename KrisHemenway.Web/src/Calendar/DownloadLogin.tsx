35d00f89e807e09b3b34deaf0bfeae7fb31f667a
Kris Hemenway <krishemenway@gmail.com>
2019-02-25 21:49:27 -0800

    fixed up styles of the login modal

--- KrisHemenway.Web/src/Calendar/DownloadLogin.tsx
+++ KrisHemenway.Web/src/Calendar/DownloadLogin.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Modal } from "../Common/Modal";
+import Modal from "../Common/Modal";
+import Text from "../Common/Text";
 import { withStyles, createStyles, Theme, WithStyles } from "@material-ui/core/styles";
 
 interface DownloadLoginParams extends WithStyles<typeof styles> {
@@ -9,6 +10,7 @@ interface DownloadLoginParams extends WithStyles<typeof styles> {
 
 interface DownloadLoginState {
 	LoginOpen: boolean;
+	Password: string;
 	ErrorText: string;
 }
 
@@ -16,11 +18,10 @@ export class DownloadLogin extends React.Component<DownloadLoginParams, Download
 	constructor(props: DownloadLoginParams) {
 		super(props);
 
-		this.passwordElement = null;
-
 		this.state = {
 			LoginOpen: false,
 			ErrorText: "",
+			Password: "",
 		};
 	}
 
@@ -31,21 +32,19 @@ export class DownloadLogin extends React.Component<DownloadLoginParams, Download
 
 		return (
 			<div className={this.props.classes.root}>
-				<a className={this.props.classes.downloadLoginLink} href="#" onClick={(() => this.setState({LoginOpen: true}))}>Login</a>
+				<a className={this.props.classes.loginLink} href="#" onClick={(() => this.setState({LoginOpen: true}))}>Login</a>
 
-				<Modal className={this.props.classes.downloadLoginForm} Open={this.state.LoginOpen}>
-					<form onSubmit={() => this.onLoginSubmitted()} style={{textAlign: "center"}}>
-						<div className="download-form-header font-24 to-upper margin-vertical bold">Password</div>
+				<Modal className={this.props.classes.loginForm} Open={this.state.LoginOpen}>
+					<form onSubmit={() => this.onLoginSubmitted()}>
+						<Text className={this.props.classes.loginHeader} Text="Password" />
 
-						<div className="margin-vertical">
-							<input tabIndex={0} className="font-16 download-login-field" type="password" ref={(element) => this.passwordElement = element} />
-						</div>
+						<input tabIndex={0} className={this.props.classes.loginPassword} type="password" onChange={(evt) => this.setState({ Password: evt.target.value })} />
 
-						{!!this.state.ErrorText ? <div className="font-16 text-center download-login-error margin-vertical">{this.state.ErrorText}</div> : ""}
+						{!!this.state.ErrorText && <Text className={this.props.classes.error} Text={this.state.ErrorText} />}
 
-						<div className="download-login-buttons text-right margin-vertical">
-							<button className="font-18 margin-horizontal" type="button" onClick={() => this.setState({LoginOpen: false})}>Cancel</button>
-							<button className="font-18 margin-horizontal" type="submit">Login</button>
+						<div className={this.props.classes.loginButtons}>
+							<button className={this.props.classes.loginButton} type="button" onClick={() => this.setState({LoginOpen: false})}>Cancel</button>
+							<button className={this.props.classes.loginButton} type="submit">Login</button>
 						</div>
 					</form>
 				</Modal>
@@ -56,36 +55,25 @@ export class DownloadLogin extends React.Component<DownloadLoginParams, Download
 	private onLoginSubmitted() {
 		this.setState({ErrorText: ""});
 
-		if (this.passwordElement === null) {
-			throw "Password element was not referenced";
-		}
-
 		$.ajax({
 			url: "/api/tvshows/episodes/authenticate",
 			method: "POST",
-			data: JSON.stringify({Password: this.passwordElement.value}),
+			data: JSON.stringify({Password: this.state.Password}),
 			contentType: "application/json; charset=utf-8",
 			dataType: "json",
 		})
 		 .done((response) => {
 			if (response.Success) {
-				this.setState({LoginOpen: false});
+				this.setState({LoginOpen: false, Password: ""});
 				this.props.OnAuthenticated();
 			} else {
-				if (this.passwordElement === null) {
-					throw "Password element was not referenced";
-				}
-
-				this.setState({ErrorText: response.ErrorMessage});
-				this.passwordElement.focus();
+				this.setState({ErrorText: response.ErrorMessage, Password: ""});
 			}
 		 })
-		 .fail((response) => {
-			this.setState({ErrorText: "Something went wrong with this request"});
+		 .fail(() => {
+			this.setState({ErrorText: "Something went wrong with this request", Password: ""});
 		 });
 	}
-
-	private passwordElement: HTMLInputElement | null;
 }
 
 const styles = (_: Theme) => createStyles({
@@ -93,11 +81,50 @@ const styles = (_: Theme) => createStyles({
 		textAlign: "center",
 		margin: "10px 0",
 	},
-	downloadLoginLink: {
-
+	loginLink: {
+		textDecoration: "none",
+		color: "#F0F0F0",
 	},
-	downloadLoginForm: {
-		padding: "10px",
+	loginForm: {
+		padding: "20px",
+		width: "400px",
+		marginLeft: "-200px",
+		background: "#F0F0F0",
+		border: "1px solid #C0C0C0",
+	},
+	loginPassword: {
+		fontSize: "16px",
+		border: "1px solid #202020",
+		padding: "3px 8px",
+		marginBottom: "20px",
+	},
+	loginButtons: {
+		textAlign: "right",
+	},
+	loginButton: {
+		fontSize: "16px",
+		color: "#101010",
+		padding: "3px 8px",
+		marginLeft: "10px",
+		background: "#E0E0E0",
+		border: "1px solid #C0C0C0",
+		cursor: "pointer",
+
+		"&:hover": {
+			background: "#D0D0D0",
+		},
+	},
+	loginHeader: {
+		fontSize: "18px",
+		display: "block",
+		color: "#101010",
+		borderBottom: "1px solid #C0C0C0",
+		padding: "0 20px 20px 20px",
+		margin: "0 -20px 20px -20px",
+	},
+	error: {
+		fontSize: "16px",
+		textAlign: "center",
 	},
 });
 
