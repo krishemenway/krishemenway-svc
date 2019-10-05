6fd58179cad587e1e633683dd23d7967280a6dfa
Kris Hemenway <krishemenway@gmail.com>
2019-10-05 00:46:37 -0700

    seperated result and resultoft into two files

--- KrisHemenway.Common/Result.cs
+++ KrisHemenway.Common/Result.cs
@@ -1,5 +1,4 @@
-﻿
-namespace KrisHemenway.Common
+﻿namespace KrisHemenway.Common
 {
 	public class Result
 	{
@@ -12,22 +11,5 @@ namespace KrisHemenway.Common
 		}
 
 		public static readonly Result Successful = new Result { Success = true, ErrorMessage = null };
-	}
-
-	public class Result<T>
-	{
-		public bool Success { get; private set; }
-		public string ErrorMessage { get; private set; }
-		public T Data { get; set; }
-
-		public static Result<T> Successful(T data)
-		{
-			return new Result<T> { Success = true, Data = data, ErrorMessage = null };
-		}
-
-		public static Result<T> Failure(string error)
-		{
-			return new Result<T> { Success = false, ErrorMessage = error };
-		}
 	}
 }
