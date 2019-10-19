26c5d28c68e38fab530a03102a9f23fcf6e87ed4
Kris Hemenway <krishemenway@gmail.com>
2019-10-18 19:39:41 -0700

    using preferred style for controller definitions

--- KrisHemenway.Notification/SentNotifications/PublicNotificationsController.cs
+++ KrisHemenway.Notification/SentNotifications/PublicNotificationsController.cs
@@ -1,29 +1,41 @@
 ﻿using KrisHemenway.Notification.PushNotifications;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace KrisHemenway.Notification.SentNotifications
 {
 	[Route("public/api")]
-	public class PublicNotificationsController : Controller
+	public class PublicNotificationsController : ControllerBase
 	{
 		[HttpGet("all")]
-		public IActionResult FindAllNotifications()
+		public ActionResult<NotificationsResponse> FindAllNotifications()
 		{
-			return Ok(new { Notifications = new NotificationStore().FindAll() });
+			return new NotificationsResponse
+			{
+				Notifications = new NotificationStore().FindAll()
+			};
 		}
 
 		[HttpGet("recent")]
-		public IActionResult FindRecentNotifications([FromQuery] DateTime fromTime)
+		public ActionResult<NotificationsResponse> FindRecentNotifications([FromQuery] DateTime fromTime)
 		{
-			return Ok(new { Notifications = new NotificationStore().FindAll(fromTime) });
+			return new NotificationsResponse
+			{
+				Notifications = new NotificationStore().FindAll(fromTime)
+			};
 		}
 
 		[HttpPost("add")]
-		public IActionResult Add([FromQuery]string deviceToken)
+		public OkResult Add([FromQuery]string deviceToken)
 		{
 			new PushRecipientStore().SaveRecipient(deviceToken);
 			return Ok();
 		}
+	}
+
+	public class NotificationsResponse
+	{
+		public IReadOnlyList<SentNotification> Notifications { get; set; }
 	}
 }
